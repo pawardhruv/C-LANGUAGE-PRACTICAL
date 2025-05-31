5c69250c0e50c749345efaca78a213d8e2076549
Dhuloo <dhruvpawarff@gmail.com>
2025-05-31 12:08:02 +0530

    Update Q5-Pattern.c

--- Q5-Pattern.c
+++ Q5-Pattern.c
@@ -1,1 +1,22 @@
  // QUE-5  Implement a C program to print the following pattern using nested for loop:
+#include<stdio.h>
+int main()
+{
+   
+    int i, j, k;
+    for(i=5 ; i>=1 ; i--)
+    {
+        for(k=1; k<=i; k++)
+        {
+            printf("  ");
+        }
+        for(j=i ; j<=5 ; j++)
+        {
+            printf(" *");
+        }
+        
+        printf("\n");
+        
+    }
+    return 0;
+}
